491212d216b92080297b5bae6ee93428fa6466f0
{五十嵐尊人} <{vgnrieee@gmail.com}>
2024-03-23 18:44:11 +0900

    todo-appの雛形部分完成

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,35 +1,105 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [inputValue, setInputValue] = useState(''); // inputValueのstateを作成。はじめは空の文字列。
+  // const [todos, setTodos] = useState<Todo[]>([]); // Todoの配列(つまりinputValue,id,checkedのみ）を持つstateを作成。はじめは空の配列。
+
+  const [todos, setTodos] = useState<Todo[]>(() => {
+    const savedTodos = localStorage.getItem('todos');
+    if (savedTodos) {
+      return JSON.parse(savedTodos);
+    } else {
+      return [];
+    }
+  });
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  type Todo = {
+    inputValue: string;
+    id: number;
+    checked: boolean;
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault(); // enterを押してもリロードされないようにする。
+    // 新しいTodoを作成する。inputValueが空の場合は何もしない。
+    if (!inputValue) return;
+    const newTodo: Todo = {
+      inputValue: inputValue,
+      id: todos.length,
+      checked: false
+    };
+    // 新しいTodoをtodosに追加する。
+    setTodos([newTodo, ...todos,]);
+    // inputValueを空にする。
+    setInputValue('');
+    // 入力欄を空にする。
+    const inputText = document.querySelector('.inputText') as HTMLInputElement;
+    inputText.value = '';
+  }
+
+  const handleEdit = (id: number, inputValue: string) => {
+    const newTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.inputValue = inputValue;
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
+  const handleChecked = (id: number, checked: boolean) => {
+    const newTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.checked = !checked;
+      }
+      return todo;
+    });
+
+    setTodos(newTodos);
+  }
+
+  const handleDelete = (id: number) => {
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='App'>
+        <h2>Todoリスト</h2>
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <input
+            type="text"
+            onChange={(e) => handleChange(e)}
+            className='inputText'
+          />
+          <input type="submit" value="作成" className='submitButton' />
+        </form>
+        <ul className='todoList'>
+          {todos.map((todo) => (
+            <li key={todo.id}>
+              <input
+                type="text"
+                onChange={(e) => handleEdit(todo.id, e.target.value)}
+                className='inputText'
+                value={todo.inputValue}
+                disabled={todo.checked}
+              />
+              <input type="checkbox" onChange={(e) => handleChecked(todo.id, todo.checked)}/>
+              <button onClick={() => handleDelete(todo.id)}>削除</button>
+            </li>
+          ))}
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
-export default App
+export default App;
